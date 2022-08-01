b0fc0f35ed1608c6bfef8506a47ad79ceb912ad8
Damien FURIET <damien@furiet.net>
2022-08-01 20:22:49 +0200

    exo 2

---
+++ App_Console/bot.swift
@@ -0,0 +1,14 @@
+//
+//  bot.swift
+//  App_Console
+//
+//  Created by Damien FURIET on 30/07/2022.
+//  Copyright © 2022 mbritto. All rights reserved.
+//
+
+import Foundation
+
+class Bot {
+    var strength = 0
+    var health = 0
+}
--- App_Console/main.swift
+++ App_Console/main.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-
+/*
 //Exemple de génération de nombre aléatoire
 let nombreAleatoire = Math.nombreAleatoire(comprisEntre: 1, et: 100)
 print(nombreAleatoire)
@@ -27,6 +27,30 @@ print("Entrez une note : ")
 let note:Double
 note = Utilisateur.saisirReel()
 print("Note : \(note)")
+*/
 
+print("Entrez votre prénom : ")
+let texte:String
+texte = Utilisateur.saisirTexte()
+
+let bot = Bot()
+bot.strength = 1
+bot.health = 100
+var i = 0
+repeat {
+    print("\(texte) : Appuyer sur entrée pour lancer les dés")
+    _ = Utilisateur.saisirTexte()
+    let de = lancerDes()
+    print("\(texte) : Dé = \(de)")
+    bot.health = bot.health - de
+    print("\(texte) : Force = \(bot.health)")
+    i += 1
+} while bot.health > 0
+print("Gagné en \(i) coups")
+
+func lancerDes() -> Int {
+    let nombreAleatoire = Math.nombreAleatoire(comprisEntre: 2, et: 12)
+    return nombreAleatoire
+}
 
 
